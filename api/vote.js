10471f2a78675f44e5b3386edfd82075055977ff
talk2miradegen-ops <talk2miradegen@gmail.com>
2026-04-25 19:56:37 -0400

    Add files via upload

---
+++ api/vote.js
@@ -0,0 +1,22 @@
+module.exports = async function handler(req, res) {
+  const { ca } = req.query;
+  const fs = require('fs');
+  const path = require('path');
+  
+  try {
+    const indexPath = path.join(process.cwd(), 'index.html');
+    let content = fs.readFileSync(indexPath, 'utf8');
+    
+    // Replace the default image with the dynamic API image route
+    const oldImage = "/images/photo_2026-04-23_21-55-06.jpg";
+    const oldImageAbsolute = "https://dexscreener-two.vercel.app/images/photo_2026-04-23_21-55-06.jpg";
+    const newImage = `https://dexscreener-two.vercel.app/api/og-image/${ca}.png`;
+    content = content.replaceAll(oldImageAbsolute, newImage).replaceAll(oldImage, newImage);
+    
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.status(200).send(content);
+  } catch (error) {
+    console.error("Error serving vote page:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
